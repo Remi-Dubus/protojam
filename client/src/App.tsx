a5f99277775bd0d048b5d64da1b48b01bc068784
rrczt974 <raphaelriviere87@gmail.com>
2024-12-03 17:27:03 +0100

    [feat] add carroussel cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
