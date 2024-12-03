590cf75a8193fa8bc0f6fdb68880fb1bba15e679
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2024-12-03 18:45:30 +0100

    added hero with title, description and shortcuts in header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -5,12 +5,24 @@ function Header() {
   return (
     <header>
       <div className={style.heroPic}>
-        <img src={hero} alt="Centre commercial" />
+        <img src={hero} alt="Centre commercial détruit." />
+      </div>
+
+      <div className={style.shortcutContainer}>
+        <a href="?">Alimentation</a>
+        <a href="?">Biens</a>
+        <a href="?">Services</a>
+        <a href="?">Équipements</a>
       </div>
 
       <div className={style.textContainer}>
         <h1>Vital Market</h1>
-        <p>Recherchez vous </p>
+      </div>
+      <div className={style.descContainer}>
+        <p>
+          Des médicaments, des vivres, de la protection ? Trouvez tout ce dont
+          vous avez besoin sur Vital Market.
+        </p>
       </div>
     </header>
   );
