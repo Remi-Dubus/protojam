45b8418c8046e210e905d33149ba50c18f6f813a
REMI DUBUS <remdubus@gmail.com>
2024-12-04 09:05:55 +0100

    feat: searchbar closes #2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import type { charactersType } from "./assets/lib/definitions";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import NavBar from "./components/navBar/NavBar";
 
 function App() {
+  // type de setCardValue
+  type SetCard = React.Dispatch<React.SetStateAction<charactersType | null>>;
+
+  // état de la carte recherchée dans la barre
+  const [cardValue, setCardValue]: [charactersType | null, SetCard] =
+    useState<charactersType | null>(null);
+
   return (
     <>
       <Header />
-      <NavBar />
-      <Outlet />
+      <NavBar setCardValue={setCardValue} />
+      <Outlet context={cardValue && cardValue} />
       <Footer />
     </>
   );
--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -1,70 +1,94 @@
+import { useState } from "react";
+import data from "../../../../server/database/characters.json";
+import type { charactersType } from "../../assets/lib/definitions";
 import style from "./searchBar.module.css";
 
-export default function SearchBar() {
-  const characters = [
-    {
-      ID: 1,
-      Prénom: "Julien",
-      Métier: "Médecin",
-      Catégorie: "Services",
-      Description:
-        "Médecin aguerri, Julien parcourt les ruines de Paris pour soigner les survivants, toujours équipé de son sac de premiers secours.",
-      Localisation: "Le Marais",
-      Image: "",
-      "Niveau de prix": "Élevé",
-      "Item/service 1": "Consultation médicale",
-      "Item/service 2": "Kit de premiers secours",
-    },
-    {
-      ID: 6,
-      Prénom: "Hugo",
-      Métier: "Armurier",
-      Catégorie: "Equipements",
-      Description:
-        "Ancien forgeron, Hugo fabrique et répare des armes pour aider les habitants à se défendre dans ce monde dangereux.",
-      Localisation: "Le Marais",
-      Image: "",
-      "Niveau de prix": "Élevé",
-      "Item/service 1": "Réparation d’armes",
-      "Item/service 2": "Vente de munitions",
-    },
-    {
-      ID: 7,
-      Prénom: "Marion",
-      Métier: "Restaurateur",
-      Catégorie: "Alimentation",
-      Description:
-        "Marion prépare des repas chauds pour les survivants, transformant des ingrédients limités en plats délicieux.",
-      Localisation: "Montmartre",
-      Image: "",
-      "Niveau de prix": "Moyen",
-      "Item/service 1": "Repas chaud",
-      "Item/service 2": "Boissons artisanales",
-    },
-    {
-      ID: 19,
-      Prénom: "Damien",
-      Métier: "Tapissier",
-      Catégorie: "Biens",
-      Description:
-        "Damien restaure et fabrique n'importe quoi à base de tissu, ramenant un peu de confort aux parisiens.",
-      Localisation: "Le Marais",
-      Image: "",
-      "Niveau de prix": "Moyen",
-      "Item/service 1": "Restauration de chaises",
-      "Item/service 2": "Création de tapisseries",
-    },
-  ];
+export default function SearchBar({
+  setCardValue,
+}: {
+  setCardValue: React.Dispatch<React.SetStateAction<charactersType | null>>;
+}) {
+  // recupétation des données
+  const characters = data;
+  // gestion de l'état
+  const [value, setValue] = useState("");
+
+  // permet d'afficher les touches entrée par l'utilisateur et remplacer les caractères interdits
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value.replace(/[^\.a-zA-Z0-9-:&()éèÉÈ']/g, ""));
+  };
+
+  // permet au click de déplacer la suggestion dans la champ de saisis
+  const onSearch = (searchTerm: string) => {
+    setValue(searchTerm);
+  };
+
+  // permet de valider la recherche
+  const onClickSearch = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    onSearch(value);
+
+    // transformation de l'entrée de l'utilisateur
+    const JobSearchCase = value.toLowerCase().replace(/\s/g, "");
+
+    // comparaison de l'entrée de l'utilisateur avec la base de donnée
+    const foundJob = characters.find(
+      (j) => j.Profession.toLowerCase().replace(/\s/g, "") === JobSearchCase,
+    );
+    // redirection
+    foundJob ? setCardValue(foundJob) : alert("not found");
+  };
+
+  // style des suggestions
+  const dropDownStyle = value !== "" ? style.dropDown : style.dropDownNone;
+
+  // filtre l'api afin de comparer l'entrée utilisateur
+  const jobFilter = characters.filter((j) => {
+    const searchTerm = value.toLowerCase().replace(/\s/g, "");
+    const JobName = j.Profession.toLowerCase().replace(/\s/g, "");
+
+    // renvoie la comparaison si il y a une recherche, si un metier est selectionné la suggestion disparait
+    return (
+      searchTerm && JobName.startsWith(searchTerm) && JobName !== searchTerm
+    );
+  });
 
   return (
     <div className={style.searchBarSection}>
       <h2 className={style.searchLabel}>Search:</h2>
       <div className={style.searchBar}>
-        <input className={style.searchInput} type="search" />
-        <button className={style.searchButton} type="button">
+        <input
+          className={style.searchInput}
+          type="search"
+          placeholder="What is your emergency?"
+          value={value}
+          onChange={onChange}
+        />
+        <button
+          className={style.searchButton}
+          type="button"
+          onClick={onClickSearch}
+        >
           {" "}
         </button>
       </div>
+      <section className={dropDownStyle}>
+        {value.length > 40 && (
+          <p className={style.error}>Too much characters.</p>
+        )}
+        {jobFilter.map((c) => (
+          <button
+            type="button"
+            key={c.ID}
+            className={style.dropDownRow}
+            onClick={() => onSearch(c.Profession)}
+          >
+            {c.Profession}
+          </button>
+        ))}
+      </section>
     </div>
   );
 }
